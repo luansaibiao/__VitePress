361fd79dcffbafdf4f6ea638d9b51d91c75ab202
“luansaibiao” <luansaibiao@163.com>
2025-01-08 15:56:15 +0800

    vite-plugin-vitepress-auto-sidebar插件配置侧边栏自动生成

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -5,6 +5,7 @@ export default {
     enhanceApp({app, router}) {
         // 单页面应用路由更新时触发百度统计事件
         router.onBeforeRouteChange = (to) => {
+             console.log('路由将改变为: ', to);
             if (typeof _hmt !== 'undefined') {
                 _hmt.push(['_trackPageview', to]);
             }
